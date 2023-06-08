6d4deb6bd9ac18ca611cac1b09de4f23158ff34f
YkorLP <levi.817weaver@gmail.com>
2023-06-08 16:58:20 -0600

    Minor Changes
    Added the ability to pickup red orb "weapon"
    Added a piss poor ability to shoot sed weapon

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
