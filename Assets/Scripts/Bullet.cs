b9a93a4b3551f2e7a8bd179e7201e18ef8ea5050
YkorLP <levi.817weaver@gmail.com>
2023-06-08 22:41:43 -0600

    Added Comments

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,11 +6,11 @@ public class Bullet : MonoBehaviour
 {
     private void Start()
     {
-        InvokeRepeating("DestroyAfterTime", 15f, 60f);
+        InvokeRepeating("DestroyAfterTime", 15f, 60f);//Starts a timer that destroys the bullet after a certain amount time
     }
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)//Checks what the bullets hits
     {
         if (collision.gameObject.tag == "Wall")
         {
